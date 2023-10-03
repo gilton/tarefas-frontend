39f69747a1d8236c21c83fbe8865d3bc3080f5d1
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2023-10-03 10:56:02 -0300

    alter: rm tarefa component reference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { AppRoutingModule } from './app.routing';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { HomeModule } from './modules/home/home.module';
 
 // Modules
-import { TarefasComponent } from './tarefas/tarefas.component';
+import { HomeModule } from './modules/home/home.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TarefasComponent,
   ],
   imports: [
     BrowserModule,
