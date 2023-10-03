018e6457d1439e8933aabb93030cea48f0cbead9
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2023-10-03 10:51:25 -0300

    feat: add header, list, delete-all, input-add-itens, home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { AppRoutingModule } from './app.routing';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeModule } from './modules/home/home.module';
+
+// Modules
+import { TarefasComponent } from './tarefas/tarefas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarefasComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
