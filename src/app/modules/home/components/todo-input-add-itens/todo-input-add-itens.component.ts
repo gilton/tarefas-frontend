101b1ceefbf9823f79c6595c4f0d00c6bb89270c
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2023-10-06 17:32:05 -0300

    feat: add ngModel and keyup.enter

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
   templateUrl: './todo-input-add-itens.component.html',
   styleUrls: ['./todo-input-add-itens.component.css']
 })
-export class TodoInputAddItensComponent {
+
+export class TodoInputAddItensComponent implements OnInit {
+  
+  @Output() emitTarefa = new EventEmitter();
+  public novaTarefa: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  public addTarefa() {
+    this.novaTarefa = this.novaTarefa.trim();
+    if( this.novaTarefa ) {
+      this.emitTarefa.emit(this.novaTarefa);
+      this.novaTarefa = "";
+    }
+  }
 
 }
